eea89c7396c1b1a39049845e65f272d9cb31603a
Vinicius <viniciusep181@gmail.com>
2023-05-27 18:41:02 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ app.listen(port, () => {
 
 const handleLogRoutes = () => {
     console.log('\x1b[92mRoutes: \x1b[33m');
-    
+    console.log('tetse')
     routes.stack.forEach(x => {
         x.route.stack.forEach((route: any) => {
             console.log(`Path:\x1b[0m ${x.route.path};\x1b[0m\x1b[32m Method:\x1b[0m ${route.method.toUpperCase()}\x1b[33m;`)
